c1c981ba663211b50066a3c1998ea4df4e0b74a1
Fabio Rehm <fgrehm@gmail.com>
2013-10-16 20:21:39 -0300

    Loops and Functions

---
+++ 24-loops-and-functions/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+const delta = 1e-15
+type sqrtFunc func(float64) (float64, int)
+
+func Sqrt(x float64) (float64, int) {
+    z := 1.0
+    for i := 0; i < 10; i++ {
+        z = z - (z * z - x) / (2 * z)
+    }
+    return z, 10
+}
+
+func SqrtDelta(x float64) (float64, int) {
+    iter := 0
+    oldZ, z := 0.0, 1.0
+    for {
+        iter++
+        z = z - (z * z - x) / (2 * z)
+        if math.Abs(z - oldZ) <= delta {
+            break
+        }
+        oldZ = z
+    }
+    return z, iter
+}
+
+func newton(funcName string, sqrt sqrtFunc) {
+    fmt.Printf("| X   | Iterations | %-21s | %-21s | %-21s |\n", funcName, "math.Sqrt", "Diff")
+    for i := 1; i <= 20; i++ {
+        x := float64(i)
+        sqrtValue, iterations := sqrt(x)
+        mathSqrt := math.Sqrt(x)
+        diff := math.Abs(mathSqrt - sqrtValue)
+        fmt.Printf("| %-3v | %-10v | %-21v | %-21v | %-21v |\n", i, iterations, sqrtValue, mathSqrt, diff)
+    }
+}
+
+func main() {
+    newton("Sqrt", Sqrt)
+    fmt.Println("------------------")
+    newton("SqrtDelta", SqrtDelta)
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ go-tour
 =======
 
 My own Tour of Go - http://tour.golang.org/
+
+To run any exercise, just type in:
+
+    go run FILE
