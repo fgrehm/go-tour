1492f8cf36a146609cb8a6b15f901feabf812d79
Fabio Rehm <fgrehm@gmail.com>
2013-10-16 22:41:34 -0300

    Maps

---
+++ 41-maps/main.go
@@ -0,0 +1,23 @@
+// Remember to `go get code.google.com/p/go-tour/wc` first
+
+package main
+
+import (
+    "code.google.com/p/go-tour/wc"
+    "fmt"
+    "strings"
+)
+
+func WordCount(s string) map[string]int {
+    ret := make(map[string]int)
+    split := strings.Fields(s)
+    for _, word := range split {
+        (ret[word])++
+    }
+    return ret
+}
+
+func main() {
+    wc.Test(WordCount)
+    fmt.Println()
+}
