fd9cc70d1db041335c3ae08074b31e6a6c123297
Fabio Rehm <fgrehm@gmail.com>
2013-10-17 13:42:14 -0300

    Fibonacci

---
+++ 44-fibonacci-closure/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+    current, previous := -1, 0
+    return func() int {
+        if current < 1 {
+            current++
+            return current
+        }
+        ret := current + previous
+        previous = current
+        current = ret
+        return ret
+    }
+}
+
+func main() {
+    f := fibonacci()
+    for i := 0; i < 21; i++ {
+        fmt.Println(f())
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,3 @@ go-tour
 =======
 
 My own Tour of Go - http://tour.golang.org/
-
-To run any exercise, just type in:
-
-    go run FILE
