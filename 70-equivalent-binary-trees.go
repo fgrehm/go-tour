41460ea9d8707ad518f7cd15b4e5d17742c5d0ba
Fabio Rehm <fgrehm@gmail.com>
2013-10-17 23:52:35 -0300

    Equivalent Binary Tree - DONE

--- 70-equivalent-binary-trees.go
+++ 70-equivalent-binary-trees.go
@@ -30,12 +30,24 @@ func walkTree(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <-ch2 {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
 	ch := make(chan int)
-	go Walk(tree.New(2), ch)
+	go Walk(tree.New(1), ch)
 	for i := range ch {
 		fmt.Println(i)
 	}
+	fmt.Println("Should return true:", Same(tree.New(1), tree.New(1)))
+	fmt.Println("Should return false:", Same(tree.New(1), tree.New(2)))
 }
