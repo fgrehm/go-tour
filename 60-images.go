9b830c7a1ab498689b26f2613474a4102b0a54bd
Fabio Rehm <fgrehm@gmail.com>
2013-10-17 22:56:58 -0300

    Images

---
+++ 60-images.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/pic"
+    "image/color"
+    "image"
+)
+
+type Image struct{
+    Width int
+    Height int
+    Color int
+}
+
+func (i Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, i.Width, i.Height)
+}
+
+func (i Image) At(x, y int) color.Color {
+    return color.RGBA{uint8(i.Color + x), uint8(i.Color + y), 255, 255}
+}
+
+func main() {
+    m := Image{150, 250, 250}
+    pic.ShowImage(m)
+}
