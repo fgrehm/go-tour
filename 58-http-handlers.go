6ea2f9ba9a54e5146965ed744a13aa0dfd7e0437
Fabio Rehm <fgrehm@gmail.com>
2013-10-17 22:38:47 -0300

    Http handlers

---
+++ 58-http-handlers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type String string
+
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
+func (str String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, str)
+}
+
+func (h *Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "%v%v %v", h.Greeting, h.Punct, h.Who)
+}
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct-go", &Struct{"Hello", ":", "Gophers!"})
+    http.Handle("/struct-ruby", &Struct{"Hello", ":", "Rubysts!"})
+    http.ListenAndServe("localhost:4000", nil)
+}
