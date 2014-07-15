06b46012c09eab5ced8f81be3792810c1a274863
Fabio Rehm <fgrehm@gmail.com>
2014-07-14 23:00:16 -0300

    Refactor slices exercise

--- 38-slices.go
+++ 38-slices.go
@@ -1,44 +1,42 @@
-// Remember to `go get code.google.com/p/go-tour/pic` first
-
 package main
 
 import "code.google.com/p/go-tour/pic"
 
-func PicMult(dx, dy int) [][]uint8 {
-	res := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		res[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			res[y][x] = uint8(x * y)
-		}
-	}
-	return res
+type funcPic func(int, int) int
+
+func picMult(x, y int) int {
+	return x * y
 }
 
-func PicHalfSum(dx, dy int) [][]uint8 {
-	res := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		res[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			res[y][x] = uint8((x + y) / 2)
-		}
-	}
-	return res
+func picHalfSum(x, y int) int {
+	return (x + y) / 2
 }
 
-func PicPow(dx, dy int) [][]uint8 {
-	res := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		res[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			res[y][x] = uint8(x ^ y)
+func picPow(x, y int) int {
+	return x ^ y
+}
+
+func picSum(x, y int) int {
+	return x + y
+}
+
+func Pic(funcForPic funcPic) (func(int, int) [][]uint8) {
+	return func(dx, dy int) [][]uint8 {
+		res := make([][]uint8, dy)
+		for y := 0; y < dy; y++ {
+			res[y] = make([]uint8, dx)
+			for x := 0; x < dx; x++ {
+				res[y][x] = uint8(funcForPic(x, y))
+			}
 		}
+		return res
 	}
-	return res
 }
 
 func main() {
-	pic.Show(PicMult)
-	pic.Show(PicHalfSum)
-	pic.Show(PicPow)
+	// Pick one at a time, use the go tour to see the images
+	pic.Show(Pic(picMult))
+	//pic.Show(Pic(picSum))
+	//pic.Show(Pic(picHalfSum))
+	//pic.Show(Pic(picPow))
 }
